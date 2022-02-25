97a7c05aabba3293705aa1ac099fe096453792f9
izumix03 <takahiro.izumikawa@gmail.com>
2022-02-25 13:50:46 +0900

    first commit

--- Package.swift
+++ Package.swift
@@ -6,18 +6,13 @@ import PackageDescription
 let package = Package(
     name: "swift-http-client",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "swift-http-client",
             targets: ["swift-http-client"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "swift-http-client",
             dependencies: []),
